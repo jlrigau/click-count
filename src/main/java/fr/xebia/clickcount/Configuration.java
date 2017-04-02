92e66f1a4e74630a82830b51e3610bdb3cd0d36f
Thomas Auffredou <tauffredou@xebia.fr>
2017-04-02 10:44:51 +0200

    Update

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -10,8 +10,13 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = "redis";
-        redisPort = 6379;
+
+        String envHost = System.getenv("CUSTOM_REDIS_HOST");
+        redisHost = (envHost != null && !envHost.isEmpty())  ? envHost : "redis";
+
+        String envPort = System.getenv("CUSTOM_REDIS_PORT");
+        redisPort = (envPort != null && !envPort.isEmpty())  ? Integer.valueOf(envPort) : 6379;
+
         redisConnectionTimeout = 2000;
     }
 }
